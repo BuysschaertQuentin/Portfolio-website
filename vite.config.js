03c3329f9045088821f85c33a052f2e3a020ffa9
unknown <quentin.buysschaert@gmail.com>
2023-05-04 01:01:53 +0200

    deploy try again

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
-// vite.config.js
 import { defineConfig } from 'vite';
-import babel from '@rollup/plugin-babel';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    babel({
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    }),
-  ],
+  base: '/Portfolio-website/',
+  plugins: [react()],
 });
