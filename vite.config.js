9021885abe7e73691e63a58a06bae67203cbee5c
unknown <quentin.buysschaert@gmail.com>
2023-05-04 00:38:43 +0200

    deploy try again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+// vite.config.js
+import { defineConfig } from 'vite';
+import babel from '@rollup/plugin-babel';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    babel({
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+  ],
+});
